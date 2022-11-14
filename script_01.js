104d2737b99e376c995cc2c45fc22e7373185836
Daniela <Daniela-Haeussler@gmx.de>
2022-11-14 09:16:33 +0100

    output + Fuktion Rechenarten

--- script_01.js
+++ script_01.js
@@ -4,7 +4,31 @@
 0. a+b / a-b/ a*b / a/b  // ergebnis c 
 1. Dateneingabe + -überprüfung :
 2. Auswahl Rechenart : 
-3. Fkt. Grundrechenarten :
-4. Ausgabe in Konsole :
+3. Fkt. Grundrechenarten : check!
+4. Ausgabe in Konsole : checke!
 */
 
+
+//module: addition a+b | test:
+
+function add(a,b) {
+return a + b;    
+};
+
+function sub(a,b) {
+    return a - b;  
+};
+
+function mult(a,b) {
+return a * b;    
+};
+
+function div(a,b) {
+return a/b; 
+};
+
+//module: console output | test
+
+function output(outputData) {
+  console.log(outputData);
+};
