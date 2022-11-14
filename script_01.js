3dcd64804e8986a2ddc8efdfa48561349ba5ba7a
Daniela <Daniela-Haeussler@gmx.de>
2022-11-14 11:48:30 +0100

    Error-Message

--- script_01.js
+++ script_01.js
@@ -3,10 +3,41 @@
 /*
 0. a+b / a-b/ a*b / a/b  // ergebnis c 
 1. Dateneingabe + -überprüfung :
-2. Auswahl Rechenart : 
+2. Auswahl Rechenart : check!
 3. Fkt. Grundrechenarten : check!
 4. Ausgabe in Konsole : checke!
 */
+
+
+
+output(calculator(2,2,"+"));
+output(calculator(2,5,"-"));
+output(calculator(4,5,"*"));
+output(calculator(6,1,":"));
+output(calculator(6,0,":"));
+
+output(calculator(9,9,"%"));
+
+function calculator(a,b,op) {
+ switch (op) {
+    case "+":
+        return add(a,b);
+
+    case "-":  
+        return substract(a,b);
+
+    case "*":
+        return multiply(a,b); 
+
+    case ":":
+    case "/":
+        return divide(a,b);
+
+    default: //ERROR
+    return "something went wrong";
+ }   
+
+}
 
 
 //module: addition a+b | test:
@@ -15,20 +46,29 @@ function add(a,b) {
 return a + b;    
 };
 
-function sub(a,b) {
+function substract(a,b) {
     return a - b;  
 };
 
-function mult(a,b) {
+function multiply(a,b) {
 return a * b;    
 };
 
-function div(a,b) {
-return a/b; 
+function divide(a,b) {
+    return (b != 0) ? a / b : "Error: Division by zero";
 };
 
 //module: console output | test
-
+//output("hello");
+//output(2);
 function output(outputData) {
-  console.log(outputData);
+
+//console.log(typeof outputData);
+
+    if (typeof outputData=="number") {
+    console.log("The result is: "+ outputData);
+    } else{
+console.log("Error: " + outputData)
+    }
+  
 };
